76fa9e51b3e5a947db37eef50e89229f55570d52
YK <yanakabakova@gmail.com>
2024-06-09 16:16:09 -0400

    set up about page

---
+++ src/pages/About/AboutPage.jsx
@@ -0,0 +1,13 @@
+import "./About.scss";
+
+
+const AboutPage = () => {
+    return (
+      <div>
+        <h1> Page</h1>
+        <p>Welcome to !</p>
+      </div>
+    );
+  };
+  
+  export default AboutPage;
