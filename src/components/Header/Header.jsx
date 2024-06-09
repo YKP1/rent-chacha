7d6b0112ca7a55f8c8d7cddddcb2afee506ce6ed
YK <yanakabakova@gmail.com>
2024-06-09 16:10:25 -0400

    minor changes to styling across pages

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,25 +1,31 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import './Header.scss';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Header.scss";
 import Logo from "../../assets/logo/MoveMountains.jpeg";
 
 const Header = () => {
   return (
     <header className="header">
       <div className="header__logo-container">
-        <img src={Logo} alt="Logo of the man pushing a mountain" className="header__logo" />
+        <img
+          src={Logo}
+          alt="Logo of the man pushing a mountain"
+          className="header__logo"
+        />
       </div>
-      <h1 className="header__title">MoveMountains</h1>
+      <Link to="/home">
+        <h1 className="header__title">MoveMountains</h1>
+      </Link>
       <nav className="header__nav-links">
-        <NavLink exact to="/home" className="header__nav-link" activeClassName="header__nav-link--active">
+        <Link to="/home" className="header__nav-link">
           Home
-        </NavLink>
-        <NavLink to="/about" className="header__nav-link" activeClassName="header__nav-link--active">
+        </Link>
+        <Link to="/about" className="header__nav-link">
           About
-        </NavLink>
-        <NavLink to="/login" className="header__nav-link" activeClassName="header__nav-link--active">
+        </Link>
+        <Link to="/login" className="header__nav-link">
           Login
-        </NavLink>
+        </Link>
       </nav>
     </header>
   );
